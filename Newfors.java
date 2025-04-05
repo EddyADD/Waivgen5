5aea38b5bd78c36163f517ddedeacb99e68e99cc
Edgar Garcia <edgar_olguin22@outlook.com>
2025-04-05 02:04:44 -0600

    Varible modificada

--- Newfors.java
+++ Newfors.java
@@ -1,6 +1,6 @@
 public class Newfors {
     public static void main(String[] args) {
-        int [] edades = {30,35,9,18,65};
+        int [] edades = {30,35,9,18,99};
         for(int i = 0; i < edades.length; i++) {
             if(edades[i] < 18) {
                 System.out.println("Lo sentimos, no puedes pasar, tu edad es " +edades[i]);
